ab3dbcff985c743ce2599f7daed103c9f4f90168
Olgittmar <olgittmar@gmail.com>
2021-12-03 01:25:15 +0100

    Compiling and running now works with cmake and g++ but cmake fails to locate gtest.

---
+++ include/MyAoC_2021/Solution_1_1.h
@@ -0,0 +1,7 @@
+#ifndef SOLUTION_1_1
+#define SOLUTION_1_1
+#include <string>
+
+int CountNumIncreasing(const std::string& str);
+
+#endif
---
+++ src/Solution_1_1.cpp
@@ -0,0 +1,7 @@
+#include "MyAoC_2021/Solution_1_1.h"
+
+int
+CountNumIncreasing(const std::string& str)
+{
+    return 0;
+}
